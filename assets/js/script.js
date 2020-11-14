38ae36d8ae51d85a0c0c8b389311f9b0d7dba957
jbeedle19 <josh.beedle@gmail.com>
2020-11-14 16:44:00 -0500

    added variable to track current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,6 @@ var dailyTasks = {};
 
 var todaysDate = moment().format("dddd, MMMM Do");
     $("#currentDay").append(todaysDate);
+
+var currentTime = moment().format("HH:mm:ss A");
+    console.log(currentTime);

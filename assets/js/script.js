dec93d24e868b9a578be9ad7e1bbdf7e3e46e987
jbeedle19 <josh.beedle@gmail.com>
2020-11-15 10:29:15 -0500

    added color to task background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-// Run everything in function when page loads
+// Run everything inside function when page loads
 $(document).ready(function() {
+    
+    // Get current date and display it
+    var todaysDate = moment().format("dddd, MMMM Do");
+        $("#currentDay").append(todaysDate);
+    
     // Listen for save button to be clicked, save tasks to localStorage 
     $(".saveBtn").on("click", function() {
         var task = $(this).siblings(".description").val();
@@ -7,14 +12,31 @@ $(document).ready(function() {
 
         localStorage.setItem(hour, task);
     });
-    var todaysDate = moment().format("dddd, MMMM Do");
-        $("#currentDay").append(todaysDate);
+    
+    // Function to check current time and color code background accordingly
+    function checkHour() {
+        var currentTime = moment().hours();
 
+        $(".time-block").each(function() {
+            var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+            // Past should be grey 
+            if (timeBlockHour < currentTime) {
+                $(this).addClass("past");
+            }
+            // Present should be red
+            else if (timeBlockHour === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            // Future should be green
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
 
-
+    checkHour();
+    localStorage.getItem(time, value);
 })
-
-
-
-var currentTime = moment().format("HH:mm:ss A");
-    console.log(currentTime);
